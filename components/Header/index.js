c1682f49c44316efe701fa1db1d3a9c81d6616e5
Truman <truman@Trumans-iMac.local>
2020-06-14 18:53:55 -0500

    Completed Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //Create Elements
+    let header = document.createElement('div');
+    let date = document.createElement('span');
+    let title = document.createElement('h1');
+    let temp = document.createElement('span');
+
+    //Styling
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //Content
+    date.textContent = `MARCH 28, 2019`;
+    title.textContent = `Lambda Times`;
+    temp.textContent = `98°`;
+    //Append
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+    
+    return header;
+}
+
+let headerContainer = document.querySelector('.header-container')
+
+headerContainer.appendChild(Header());
